0cec1000a707d12c58695808602a4af7a6019e19
tharequest <thareqabdulaziz@fmipa.untan.ac.id>
2026-02-06 10:58:11 +0700

    update homepage ui

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="bg-gray-900 text-white text-center p-6 mt-10">
-        <p>© 2026 EduPlatform. All rights reserved.</p>
+        <p>© 2026 th-kelasdata. All rights reserved.</p>
       </footer>
     </div>
   );
